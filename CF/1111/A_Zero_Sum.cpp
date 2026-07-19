e69799ee12f9add8acab35390f129dc08c29772f
JMSbro <jmsmahruf@gmail.com>
2026-07-19 15:19:47 +0600

    New Solutions Uploaded for cf 1111 and dfs practice

---
+++ CF/1111/A_Zero_Sum.cpp
@@ -0,0 +1,234 @@
+
+//In the name of ALLAH
+
+#include<bits/stdc++.h>
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// #include <map>
+// #include <set>
+// #include <iomanip>
+// #include <cmath>
+// #include <cstring>
+// #include <queue>
+// #include <stack>
+// #include <utility>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+//use less_equal for ordered multiset
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define pb push_back
+#define F first
+#define S second
+#define mp make_pair
+#define endl '\n'
+#define all(a) (a).begin(),(a).end()
+#define sz(x) (int)x.size()
+#define mid(l,r) (l + (r - l) / 2)
+#define left(node) (node*2)
+#define right(node) (node*2+1)
+#define mx_int_prime 999999937
+#define  yes  cout << "YES\n";
+#define  no  cout << "NO\n";
+#define fill(x) for(auto &u: x) cin >> u;
+#define allprint(x) for(auto u: x) cout << u << ' '; cout << endl;
+#define print(x) cout << x << endl;
+#define f(i, j, n, k) for (int i = j; i < n; i = i + k)
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a,b) memset(a, b, sizeof(a) )
+//#define gcd(a,b) __gcd(a,b)
+#define sqr(a) ((a) * (a))
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define frac(n) cout << fixed << setprecision(n)
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+typedef vector<int>::iterator vit;
+typedef set<int>::iterator sit;
+
+
+int dx[] = {0, 0, +1, -1};
+int dy[] = {+1, -1, 0, 0};
+//int dx[] = {+1, 0, -1, 0, +1, +1, -1, -1};
+//int dy[] = {0, +1, 0, -1, +1, -1, +1, -1};
+
+
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+            return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+            os << "{";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "]";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const multiset< T > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << it -> first << " = " << it -> second ;
+                                            }
+                    return os << "]";
+}
+
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+
+void faltu () {
+            cerr << endl;
+}
+
+template <typename T>
+void faltu( T a[], int n ) {
+            for(int i = 0; i < n; ++i) cerr << a[i] << ' ';
+                cerr << endl;
+}
+
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest) {
+            cerr << arg << ' ';
+                faltu(rest...);
+}
+
+/// no debugger for stack , queue and priority queue. also no iterator for them
+/// comment cin >> t for single test case
+
+void solve(){
+    int n; cin >> n;
+    vi v(n); 
+    fill(v);
+
+    int cnto = 0, cntn = 0;
+    for(int i = 0; i < n; i++){
+        if(v[i] == 1) cnto++;
+        else cntn++;
+    }
+
+    int diff = abs(cnto - cntn);
+    if(n&1) {
+        no
+        return;
+    }
+    int needo = n/2, needn = n/2;
+    if(cnto > needo) diff = abs(cnto - needo);
+    else if(cntn > needn) diff = abs(cntn - needn);
+
+    ll cn = 0, co = 0;
+    for(int i = 0; i < n - 1; i++){
+        if(v[i] == v[i+1]){
+            if(v[i] == 1){
+                co+=2;
+            }
+            else cn+=2;
+            i++;
+        }
+    }
+
+    if(cnto == cntn) yes
+    else if(cnto > cntn){
+        if(co >= diff && diff % 2 == 0) yes
+        else no
+    }
+    else {
+        if(cn >= diff && diff % 2 == 0) yes
+        else no
+    }
+    // if(sum&1) {
+    //     no
+    //     return;
+    // }
+
+    // if(!sum){
+    //     yes
+    //     return;
+    // }
+
+    // if(sum > 0){
+    //     if(sum == co){
+    //         yes
+    //     }
+    //     else no
+    // }
+    // else{
+    //     if(sum == cn) yes
+    //     else no
+    // }
+}
+
+int main()
+{
+    optimize();
+    int t = 1;
+    cin >> t;
+    // if(t == 99858){
+    //   for(int i = 1; i <= t; i++){
+    //     if(i == 27556) {
+    //         int n,k;
+    //         cin >> n >> k;
+    //         cout << n << k;
+    //     }
+    //     else {
+    //         int n,k;
+    //         cin >> n >> k;
+    //     }
+    //   }
+    // }
+    // else {
+    //     while(t--){
+    //         solve();
+    //     }
+    // }
+    while(t--){
+        solve();
+    }
+    return 0;
+}
---
+++ CF/1111/C_Inversion_of_a_Subsequence.cpp
@@ -0,0 +1,212 @@
+
+//In the name of ALLAH
+
+#include<bits/stdc++.h>
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// #include <map>
+// #include <set>
+// #include <iomanip>
+// #include <cmath>
+// #include <cstring>
+// #include <queue>
+// #include <stack>
+// #include <utility>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+//use less_equal for ordered multiset
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define pb push_back
+#define F first
+#define S second
+#define mp make_pair
+#define endl '\n'
+#define all(a) (a).begin(),(a).end()
+#define sz(x) (int)x.size()
+#define mid(l,r) (l + (r - l) / 2)
+#define left(node) (node*2)
+#define right(node) (node*2+1)
+#define mx_int_prime 999999937
+#define  yes  cout << "YES\n";
+#define  no  cout << "NO\n";
+#define fill(x) for(auto &u: x) cin >> u;
+#define allprint(x) for(auto u: x) cout << u << ' '; cout << endl;
+#define print(x) cout << x << endl;
+#define f(i, j, n, k) for (int i = j; i < n; i = i + k)
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a,b) memset(a, b, sizeof(a) )
+//#define gcd(a,b) __gcd(a,b)
+#define sqr(a) ((a) * (a))
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define frac(n) cout << fixed << setprecision(n)
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+typedef vector<int>::iterator vit;
+typedef set<int>::iterator sit;
+
+
+int dx[] = {0, 0, +1, -1};
+int dy[] = {+1, -1, 0, 0};
+//int dx[] = {+1, 0, -1, 0, +1, +1, -1, -1};
+//int dy[] = {0, +1, 0, -1, +1, -1, +1, -1};
+
+
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+            return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+            os << "{";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "]";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const multiset< T > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << it -> first << " = " << it -> second ;
+                                            }
+                    return os << "]";
+}
+
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+
+void faltu () {
+            cerr << endl;
+}
+
+template <typename T>
+void faltu( T a[], int n ) {
+            for(int i = 0; i < n; ++i) cerr << a[i] << ' ';
+                cerr << endl;
+}
+
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest) {
+            cerr << arg << ' ';
+                faltu(rest...);
+}
+
+/// no debugger for stack , queue and priority queue. also no iterator for them
+/// comment cin >> t for single test case
+
+void solve(){
+    int n; cin >> n; 
+    vi a(n), b(n);
+    fill(a); fill(b);
+
+    if(a == b) {
+        cout << 0 << endl;
+        return;
+    }
+
+    int oz = 0, oo = 0, zz = 0, change = 0, allz = 1; 
+    for(int i = 0; i < n; i++){
+        if(a[i] == 1) allz = 0;
+    }
+    if(allz){
+        cout << -1 << endl; return;
+    }
+
+    for(int i = 0; i < n; i++){
+        if(a[i] == 1 && b[i] == 0){
+            oz = 1;
+            change++;
+        }
+        else if(a[i] == 0 && b[i] == 0) zz++;
+        else if(a[i] == 1 && b[i] == 1) oo++;
+    }
+    if(oz){
+        if(change&1) cout << 1 << endl;
+        else cout << 2 << endl;
+        return;
+    }
+
+    if(oo && zz) cout << 2 << endl;
+    else cout << -1 << endl;
+
+}
+
+int main()
+{
+    optimize();
+    int t = 1;
+    cin >> t;
+    // if(t == 10000){
+    //   for(int i = 1; i <= t; i++){
+    //     if(i == 32) {
+    //         int n; 
+    //         vi a(n), b(n);
+    //         fill(a); fill(b);
+    //         cout  << a << endl;
+    //         cout << b << endl;
+    //     }
+    //     else {
+    //         int n; 
+    //         vi a(n), b(n);
+    //         fill(a); fill(b);
+    //     }
+    //   }
+    // }
+    // else {
+    //     while(t--){
+    //         solve();
+    //     }
+    // }
+    while(t--){
+        solve();
+    }
+    return 0;
+}
