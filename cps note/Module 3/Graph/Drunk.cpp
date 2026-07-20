b7b8b993e18600e6c417d6925dbcfc3754a2a79c
JMSbro <jmsmahruf@gmail.com>
2026-07-20 10:46:16 +0600

    new codes

---
+++ cps note/Module 3/Graph/Drunk.cpp
@@ -0,0 +1,264 @@
+
+//In the name of ALLAH
+
+#include<bits/stdc++.h>
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// #include <map>
+// #include <set>
+// #include <iomanip>
+// #include <cmath>
+// #include <cstring>
+// #include <queue>
+// #include <stack>
+// #include <utility>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+//use less_equal for ordered multiset
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define pb push_back
+#define F first
+#define S second
+#define mp make_pair
+#define endl '\n'
+#define all(a) (a).begin(),(a).end()
+#define sz(x) (int)x.size()
+#define mid(l,r) (l + (r - l) / 2)
+#define left(node) (node*2)
+#define right(node) (node*2+1)
+#define mx_int_prime 999999937
+#define  yes  cout << "YES\n";
+#define  no  cout << "NO\n";
+#define fill(x) for(auto &u: x) cin >> u;
+#define allprint(x) for(auto u: x) cout << u << ' '; cout << endl;
+#define print(x) cout << x << endl;
+#define f(i, j, n, k) for (int i = j; i < n; i = i + k)
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a,b) memset(a, b, sizeof(a) )
+//#define gcd(a,b) __gcd(a,b)
+#define sqr(a) ((a) * (a))
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define frac(n) cout << fixed << setprecision(n)
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+typedef vector<int>::iterator vit;
+typedef set<int>::iterator sit;
+
+
+int dx[] = {0, 0, +1, -1};
+int dy[] = {+1, -1, 0, 0};
+//int dx[] = {+1, 0, -1, 0, +1, +1, -1, -1};
+//int dy[] = {0, +1, 0, -1, +1, -1, +1, -1};
+
+
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+            return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+            os << "{";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "]";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const multiset< T > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << it -> first << " = " << it -> second ;
+                                            }
+                    return os << "]";
+}
+
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+
+void faltu () {
+            cerr << endl;
+}
+
+template <typename T>
+void faltu( T a[], int n ) {
+            for(int i = 0; i < n; ++i) cerr << a[i] << ' ';
+                cerr << endl;
+}
+
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest) {
+            cerr << arg << ' ';
+                faltu(rest...);
+}
+
+/// no debugger for stack , queue and priority queue. also no iterator for them
+/// comment cin >> t for single test case
+const int N = 1e5+5;
+map<string, int> state;
+map<string, vector<string>> ma; 
+set<string> s; 
+int ok = 1;
+
+void dfs(string node){  ///Prev node pass kora lagena ei code e
+    // if(state[node]){
+    //     ok = 0;
+    //     return;//MOST IMPORTANT
+    // }
+
+    state[node] = 1; 
+    for(auto &u: ma[node]){
+        //if(u != prev){
+            if(state[u] == 1){ok = 0; return;}//MOST IMPORTANT
+            if(state[u] == 0) dfs(u);
+        
+    }
+    state[node] = 2;
+}
+
+void solve(){
+    //v.clear();
+    ma.clear();
+    state.clear();
+    s.clear();
+    ok = 1; 
+    int m; cin >> m;
+    int tmp = m;
+
+    while(m--){
+        string a, b; cin >> a >> b;
+        ma[a].pb(b);
+        // ma[b].pb(a);
+        s.insert(a); s.insert(b);
+    }
+    for(auto it: s){
+        if(!state[it]) dfs(it);
+    }
+     //dbg(s, ma);
+    // dbg(vis);
+
+    if(ok) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
+
+
+// #include<bits/stdc++.h>
+ 
+// using namespace std;
+ 
+// #define N 200005
+ 
+// int n, m;
+// vector<int> adj[N];
+// int previous_node[N];
+// int visited[N];
+// bool cycle_found = false;
+ 
+// void dfs(int u, int prev){
+//     if(visited[u]){
+//         if(cycle_found==false){
+//             cycle_found = true;
+//         }
+//         return;
+//     }
+ 
+//     visited[u] = 1;
+//     previous_node[u] = prev;
+ 
+//     for(int v: adj[u]){
+//         dfs(v, u);
+//     }
+// }
+ 
+// int main() {
+//     cin >> n >> m;
+ 
+//     for(int i = 0; i < m; i++){
+//         int u, v;
+//         cin >> u >> v;
+ 
+//         adj[u].push_back(v);
+//     }
+ 
+//     for(int i = 1; i <= n; i++){
+//         if(!visited[i]) dfs(i, 0);
+//     }
+//     return 0;
+// }
+
+int main()
+{
+    optimize();
+    int t = 1;
+    cin >> t;
+    // if(t == 99858){
+    //   for(int i = 1; i <= t; i++){
+    //     if(i == 27556) {
+    //         int n,k;
+    //         cin >> n >> k;
+    //         cout << n << k;
+    //     }
+    //     else {
+    //         int n,k;
+    //         cin >> n >> k;
+    //     }
+    //   }
+    // }
+    // else {
+    //     while(t--){
+    //         solve();
+    //     }
+    // }
+    for(int i = 1; i <= t; i++){
+        cout << "Case " << i << ": ";
+        solve();
+    }
+    return 0;
+}
