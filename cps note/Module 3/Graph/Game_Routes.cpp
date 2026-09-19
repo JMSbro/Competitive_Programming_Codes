87f5bec508815ac2848a4f9d2d2c2bde070f73e7
JMSbro <jmsmahruf@gmail.com>
2026-09-19 23:00:49 +0600

    new solves

---
+++ cps note/Module 3/Graph/Game_Routes.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define N 200005
+
+int n, m;
+vector<int> adj[N];
+int visited[N];
+vector<int> topological_order;
+
+void dfs(int u){
+    visited[u] = 1;
+
+    for(int v: adj[u]){
+        if(visited[v]) continue;
+        dfs(v);
+    }
+
+    topological_order.push_back(u);
+}
+
+void topological_sort(){
+    topological_order.clear();
+    for(int i = 1; i <= n; i++){
+        if(visited[i]==0){
+            dfs(i);
+        }
+    }
+
+    reverse(topological_order.begin(), topological_order.end());
+}
+
+int main() {
+    cin >> n >> m;
+
+    for(int i = 1; i <= m; i++){
+        int u, v;
+        cin >> u >> v;
+
+        adj[u].push_back(v);
+    }
+
+    topological_sort();
+
+    int ans[n+2];
+    memset(ans, 0, sizeof ans);
+
+    ans[n] = 1;
+
+    for(int i = n-1; i >= 0; i--){
+        int u = topological_order[i];
+
+        if(u==n){
+            continue;
+        }
+
+        int sum = 0;
+
+        for(int v: adj[u]){
+            sum += ans[v];
+            sum %= 1000000007;
+        }
+
+        ans[u] = sum;
+    }
+
+    cout << ans[1] << endl;
+    
+    return 0;
+}
